e4164e7b30a41355ababd1d048a3cc4202333ce5
subeomi <tnqjarnqja@gmail.com>
2023-07-07 18:18:22 +0900

    0707

--- src/main/java/org/zerock/j1/service/ReplyServiceImpl.java
+++ src/main/java/org/zerock/j1/service/ReplyServiceImpl.java
@@ -31,29 +31,40 @@ public class ReplyServiceImpl implements ReplyService {
         
         boolean last = requestDTO.isLast();
 
+        // 현재 페이지
         int pageNum = requestDTO.getPage();
 
+        // 마지막 페이지를 요청할 경우?
         if(last){
+            // replyRepository에서 해당 bno 게시물의 전체 댓글 수를 가져온다
             long totalCount = replyRepository.getCountBoard(requestDTO.getBno());
 
+            // 전체 댓글 수를 size로 나눠서 총 페이지 수를 계산한 후 pageNum에 넣음 예) 52/50.0 = 1.04 -> 올리면 2
             pageNum = (int)(Math.ceil(totalCount/(double)requestDTO.getSize()));
         }
 
+        // 페이지어블 생성. PageRequest.of(페이지 번호, 페이지의 사이즈, 정렬 기준(rno로 오름차순 어센딩)
         Pageable pageable = PageRequest.of(pageNum -1, requestDTO.getSize(), Sort.by("rno").ascending());
 
+        // pageable의 결과물인 Page타입 객체. 해당 bno의 해당 댓글페이지의 댓글을 가져온다
         Page<Reply> result = replyRepository.listBoard(requestDTO.getBno(), pageable);
 
         log.info("-----------------------------------");
 
+        // 가져온 댓글인 Page타입 result의 총 댓글 수(getTotalElements)를 가져와 저장
         long totalReplyCount = result.getTotalElements();
 
+        // 가져온 댓글 데이터를 List<ReplyDTO>타입으로 가공한다.
         List<ReplyDTO> dtoList = result.get()
         .map(en -> modelMapper.map(en, ReplyDTO.class))
         .collect(Collectors.toList());
 
+        // 가공된 dtoList를 이용하여 PageResponseDTO를 생성 (댓글데이터, 총댓글수, ReplyPageRequestDTO(bno 페이지 사이즈 last))
         PageResponseDTO<ReplyDTO> responseDTO = new PageResponseDTO<>(dtoList, totalReplyCount, requestDTO);
+        // PageResponseDTO의 페이지번호를 pageNum로 설정함
         responseDTO.setPage(pageNum);
 
+        // 최종 댓글 데이터 반환
         return responseDTO;
     }
     
